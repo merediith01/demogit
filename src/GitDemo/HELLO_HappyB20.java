bbd19ef5c8ab54aaf95ec6720ddf04c91a74fce9
merediith01 <merediith01@gmail.com>
2020-08-01 12:05:13 -0400

    demoPractice

---
+++ src/GitDemo/HELLO_HappyB20.java
@@ -0,0 +1,11 @@
+package GitDemo;
+
+public class HELLO_HappyB20 {
+    public static void main(String[] args) {
+
+        System.out.println(" HELLO B20 ");
+
+        // a new line is been added
+
+    }
+}
