fe02999c66acc5db0966f9d07ce8444f2a0208ac
merediith01 <merediith01@gmail.com>
2020-08-01 15:48:36 -0400

    ourself.java

---
+++ src/GitDemo/byMyself.java
@@ -0,0 +1,10 @@
+package GitDemo;
+
+public class byMyself {
+    public static void main(String[] args) {
+
+        System.out.println("Hola mundo");
+        System.out.println("Hello World");
+
+    }
+}
