6b978542f47e34cf628d627204ba661f58b25353
merediith01 <merediith01@gmail.com>
2020-08-01 12:45:17 -0400

    update

--- src/GitDemo/happyClass.java
+++ src/GitDemo/happyClass.java
@@ -1,7 +1,8 @@
 package GitDemo;
 
-public class HELLO_HappyB20 {
+public class happyClass {
     public static void main(String[] args) {
+
 
         System.out.println(" HELLO B20 ");
         System.out.println("testing practice");
